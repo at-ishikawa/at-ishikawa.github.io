bbc6173faa4dee89ab4373581446b972dedc01af
A. Ishikawa <10303962+at-ishikawa@users.noreply.github.com>
2020-03-06 23:32:57 -0800

    Go/gRPC: Add server streaming example

--- examples/golang/grpc/client/main.go
+++ examples/golang/grpc/client/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"time"
 
 	pb "github.com/at-ishikawa/at-ishikawa.github.io/examples/golang/grpc/protos"
 	"golang.org/x/net/context"
@@ -13,6 +16,49 @@ const (
 	grpcPort   = ":50051"
 )
 
+func SayHello(client pb.HelloWorldClient) error {
+	response, err := client.SayHello(context.Background(), &pb.HelloRequest{
+		Name: "client name",
+	})
+	if err != nil {
+		return err
+	}
+	fmt.Printf("response: %v\n", response)
+	return nil
+}
+
+func KeepGettingHello(client pb.HelloWorldClient) error {
+	startTime := time.Now()
+	stream, err := client.KeepReplyingHello(context.Background(), &pb.HelloRequest{
+		Name: "client name",
+	})
+	if err != nil {
+		return err
+	}
+	md, err := stream.Header()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("header: %+v\n", md)
+
+	for {
+		response, err := stream.Recv()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Printf("failed to receive some response: %+v\n", err)
+			return stream.CloseSend()
+		}
+		fmt.Printf("response: %v\n", response)
+		// <-time.After(100 * time.Millisecond)
+	}
+	md = stream.Trailer()
+	duration := time.Now().Sub(startTime)
+	fmt.Printf("trailer: %+v\n", md)
+	fmt.Printf("client duration: %+v\n", duration)
+	return nil
+}
+
 func main() {
 	opts := []grpc.DialOption{
 		grpc.WithInsecure(),
@@ -20,17 +66,17 @@ func main() {
 	conn, err := grpc.Dial(serverAddr+grpcPort, opts...)
 	if err != nil {
 		fmt.Printf("Failed to dial: %v\n", err)
-		return
+		os.Exit(1)
 	}
 	defer conn.Close()
 	client := pb.NewHelloWorldClient(conn)
-
-	response, err := client.SayHello(context.Background(), &pb.HelloRequest{
-		Name: "client name",
-	})
-	if err != nil {
-		fmt.Printf("Failed to say hello: %v\n", err)
-		return
+	if err := SayHello(client); err != nil {
+		fmt.Printf("main > SayHello error: %+v\n", err)
+		os.Exit(1)
 	}
-	fmt.Printf("response: %v\n", response)
+	if err := KeepGettingHello(client); err != nil {
+		fmt.Printf("main > KeepGettingHello error: %+v\n", err)
+		os.Exit(1)
+	}
+	os.Exit(0)
 }
--- examples/golang/grpc/server/main.go
+++ examples/golang/grpc/server/main.go
@@ -2,22 +2,57 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
+	"time"
 
-	pb "github.com/at-ishikawa/at-ishikawa.github.io/examples/golang/grpc/protos"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
+
+	pb "github.com/at-ishikawa/at-ishikawa.github.io/examples/golang/grpc/protos"
 )
 
 const (
 	grpcPort = ":50051"
 )
 
-type Server struct{}
+type Server struct {
+	pb.HelloWorldServer
+}
 
-func (s *Server) SayHello(context context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+func (s *Server) SayHello(_ context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+	log.Printf("receive name: %s in SayHello", in.Name)
 	return &pb.HelloResponse{Message: "Hello " + in.Name}, nil
+}
+
+func (s *Server) KeepReplyingHello(in *pb.HelloRequest, stream pb.HelloWorld_KeepReplyingHelloServer) error {
+	startTime := time.Now()
+	if err := stream.SetHeader(metadata.New(map[string]string{
+		"start": startTime.Format(time.RFC3339),
+	})); err != nil {
+		log.Printf("err to send header: %+v", err)
+		return status.Errorf(codes.Internal, "error to set header: %+v", err)
+	}
+
+	for i := 1; i < 10; i++ {
+		err := stream.Send(&pb.HelloResponse{
+			Message: fmt.Sprintf("%d: Hello %s", i, in.Name),
+		})
+		if err != nil {
+			log.Printf("error: %+v", err)
+			return status.Errorf(codes.Internal, "error to Send: %+v", err)
+		}
+	}
+	duration := time.Now().Sub(startTime)
+	stream.SetTrailer(metadata.New(map[string]string{
+		"duration": duration.String(),
+	}))
+	log.Printf("receive name: %s in KeepReplyingHello, took: %d", in.Name, duration)
+	return nil
 }
 
 func main() {
